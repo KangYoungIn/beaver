c2905aca3832179c75ed1fbadc4548c4a16feb6e
YOUNGIN KANG <yikang1102@gmail.com>
2019-06-06 19:10:19 +0900

    190606-scrapetest

--- example/scrapetest.py
+++ example/scrapetest.py
@@ -1,7 +1,5 @@
 #!/bin/env python
 
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
 html = urlopen("http://www.mapcloud.co.kr")
-bs0bj = BeautifulSoup(html.read(), "html.parser")
-print(bs0bj.h1)
+print(html.read())
