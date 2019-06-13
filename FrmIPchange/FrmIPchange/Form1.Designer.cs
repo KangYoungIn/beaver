37f773a0c1a2d45690befc7ffec0606d7f64044a
sangmyungjiwon <julia1140@naver.com>
2019-06-13 10:37:15 +0900

    190613-윤지원-1

---
+++ FrmIPchange/FrmIPchange/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace FrmIPchange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDNS2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDNS1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSubnetMask = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGateWay = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "자동IP발번";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(135, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "수동IP발번";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(10, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "* 수동IP발번 시 하단정보 입력해주세요";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDNS2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtDNS1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSubnetMask);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtIP);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtGateWay);
+            this.groupBox1.Location = new System.Drawing.Point(11, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 150);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "수동IP 설정";
+            // 
+            // txtDNS2
+            // 
+            this.txtDNS2.Enabled = false;
+            this.txtDNS2.Location = new System.Drawing.Point(93, 123);
+            this.txtDNS2.Name = "txtDNS2";
+            this.txtDNS2.ReadOnly = true;
+            this.txtDNS2.Size = new System.Drawing.Size(139, 21);
+            this.txtDNS2.TabIndex = 13;
+            this.txtDNS2.Text = "168.126.63.2";
+            this.txtDNS2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "DNS";
+            // 
+            // txtDNS1
+            // 
+            this.txtDNS1.Enabled = false;
+            this.txtDNS1.Location = new System.Drawing.Point(93, 97);
+            this.txtDNS1.Name = "txtDNS1";
+            this.txtDNS1.ReadOnly = true;
+            this.txtDNS1.Size = new System.Drawing.Size(139, 21);
+            this.txtDNS1.TabIndex = 11;
+            this.txtDNS1.Text = "168.126.63.1";
+            this.txtDNS1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "SubNetMask";
+            // 
+            // txtSubnetMask
+            // 
+            this.txtSubnetMask.Location = new System.Drawing.Point(93, 72);
+            this.txtSubnetMask.Name = "txtSubnetMask";
+            this.txtSubnetMask.ReadOnly = true;
+            this.txtSubnetMask.Size = new System.Drawing.Size(139, 21);
+            this.txtSubnetMask.TabIndex = 9;
+            this.txtSubnetMask.Text = "255.255.255.0";
+            this.txtSubnetMask.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(93, 19);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(139, 21);
+            this.txtIP.TabIndex = 7;
+            this.txtIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "GateWay";
+            // 
+            // txtGateWay
+            // 
+            this.txtGateWay.Location = new System.Drawing.Point(93, 45);
+            this.txtGateWay.Name = "txtGateWay";
+            this.txtGateWay.Size = new System.Drawing.Size(139, 21);
+            this.txtGateWay.TabIndex = 5;
+            this.txtGateWay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 244);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "IP 변경 프로그램";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSubnetMask;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGateWay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDNS1;
+        private System.Windows.Forms.TextBox txtDNS2;
+    }
+}
+
---
+++ FrmIPchange/FrmIPchange/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Net;
+namespace FrmIPchange
+{
+    public partial class Form1 : Form
+    {
+        string sAdapterDsc = string.Empty; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            Set_adapters();
+        }
+
+        public void Set_adapters()
+        {
+            sAdapterDsc = ""; 
+
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces(); 
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (adapter.NetworkInterfaceType.ToString() == "Ethernet")
+                {
+                    // Adapter 이름
+                    sAdapterDsc = adapter.Description; 
+
+                    // 기존컴퓨터 DNS 코드 가져오기 (참고)
+                    //if (adapter.OperationalStatus == OperationalStatus.Up)
+                    //{
+                    //    IPInterfaceProperties ipProperties = adapter.GetIPProperties();
+                    //    IPAddressCollection dnsAddresses = ipProperties.DnsAddresses;
+
+                    //    int i = 0;
+
+                    //    foreach (IPAddress dnsAdress in dnsAddresses)
+                    //    {
+                    //        if (i == 0)
+                    //        {   
+                    //            txtDNS1.Text = dnsAdress.ToString(); // 기본 설정 DNS 서버
+                    //        }
+                    //        else
+                    //        {
+                    //            txtDNS2.Text = dnsAdress.ToString(); // 보조 DNS 서버
+
+                    //        }
+                    //        i++; 
+                    //    }
+                    //}
+                }
+            } 
+        }
+
+        public void setIP수동(string IPAddress, string Gateway, string SubnetMask, string DNS) 
+        { 
+            var adapterConfig = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            var networkCollection = adapterConfig.GetInstances();
+
+            foreach (ManagementObject adapter in networkCollection)
+            {
+                string description = adapter["Description"] as string;
+                if (string.Compare(description, sAdapterDsc, StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    try
+                    {
+                        // Gateway
+                        var newGateway = adapter.GetMethodParameters("SetGateways");
+                        newGateway["DefaultIPGateway"] = new string[] { Gateway };
+                        newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                        // IP
+                        var newAddress = adapter.GetMethodParameters("EnableStatic");
+                        newAddress["IPAddress"] = new string[] { IPAddress };
+                        newAddress["SubnetMask"] = new string[] { SubnetMask };
+
+                        // DNS
+                        var newDNS = adapter.GetMethodParameters("SetDNSServerSearchOrder");
+                        newDNS["DNSServerSearchOrder"] = DNS.Split(',');
+
+                        adapter.InvokeMethod("EnableStatic", newAddress, null);
+                        adapter.InvokeMethod("SetGateways", newGateway, null);
+                        adapter.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+                        MessageBox.Show("수동IP가 설정되었습니다.", "확인", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                    catch (Exception ex)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void setIP자동()
+        { 
+            var adapterConfig = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            var networkCollection = adapterConfig.GetInstances();
+
+            foreach (ManagementObject adapter in networkCollection)
+            {
+                string description = adapter["Description"] as string;
+                if (string.Compare(description, sAdapterDsc, StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    try
+                    {
+                        adapter.InvokeMethod("EnableDHCP", null);
+                        MessageBox.Show("자동IP가 설정되었습니다.", "확인", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                    catch (Exception ex)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setIP자동();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Chk_Info();
+            setIP수동(txtIP.Text.Trim(), txtGateWay.Text.Trim(), txtSubnetMask.Text.Trim(), txtDNS1.Text.Trim() + ',' + txtDNS2.Text.Trim()); // IP, Gateway, SubnetMask(고정) 
+        }
+
+        public void Chk_Info()
+        {
+            if (txtIP.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("IP를 입력해주세요.", "확인", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (txtSubnetMask.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("SubnetMask를 입력해주세요.", "확인", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+        }
+
+         
+    }
+}
