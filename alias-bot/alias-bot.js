1ebbb27db31d930928666d0497bc7edb97a15ffa
kangyoungin <yikang1102@gmail.com>
2019-06-22 03:10:57 +0900

    토큰을 공개하면?

---
+++ alias-bot/alias-bot.js
@@ -0,0 +1,42 @@
+const botkit = require('botkit');  //봇 모듈
+const slack = require('slack-node');
+
+const controller = botkit.slackbot({
+    debug: false,
+    log: true
+});
+
+const botScope = [
+    'direct_message',
+    'direct_mention',
+    'metion'
+];
+
+controller.hears(['위키', 'wiki'], botScope, (bot, message) => {
+    bot.reply(message, 'http://wiki.mapcloud.co.kr');
+});
+
+controller.hears(['깃헙', '깃허브', 'github'], botScope, (bot, message) => {
+    bot.reply(message, 'https://github.com/kangyoungin/beaver.git');
+});
+
+controller.hears(['putty', '푸티'], botScope, (bot, message) => {
+    bot.reply(message, 'https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html');
+});
+
+controller.hears(['mremote', '원격접속툴'], botScope, (bot, message) => {
+    bot.reply(message, 'https://www.mapcloud.co.kr/index.php/s/wOCwLSxm1V68rhT/download');
+});
+
+controller.hears(['됴니생일', '지원이생일'], botScope, (bot, message) => {
+    bot.reply(message, '1994년 11월 4일입니다.');
+});
+
+
+controller.hears(['요니생일', '영인이생일'], botScope, (bot, message) => {
+    bot.reply(message, '1992년 11월 2일입니다.');
+});
+
+controller.spawn({
+    token: 'xoxb-661033479312-661267107826-YHASH7b0PUudqYJtc9IHORkx'
+}).startRTM();
